3d23f54bc82c501945423cc150803766ef279200
RKY <xraj2023@gmail.com>
2025-01-01 14:57:07 +0530

    env variables added

--- pages/api/getMeetup.js
+++ pages/api/getMeetup.js
@@ -6,7 +6,7 @@ async function getMeetup (req, res) {
     
     // const { title, image, address, description } = data;
     const client = await MongoClient.connect(
-        'mongodb+srv://xraj2023:LyfJlpM2nZlWQNh4@cluster0.i4rdwsy.mongodb.net/meetups?retryWrites=true&w=majority&appName=Cluster0'
+        process.env.MONGO_URL
     );
     const db = client.db();
 
--- pages/api/hello.js
+++ pages/api/hello.js
@@ -17,7 +17,7 @@ async function myapi (req, res) {
     // 
     
     const { title, image, address, description } = data;
-    const client = await MongoClient.connect('mongodb+srv://xraj2023:LyfJlpM2nZlWQNh4@cluster0.i4rdwsy.mongodb.net/meetups?retryWrites=true&w=majority&appName=Cluster0');
+    const client = await MongoClient.connect(process.env.MONGO_URL);
     const db = client.db();
 
     const meetupsCollection = db.collection('meetups');
